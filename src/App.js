3a829de62feb0f19ef7913dd309e5534c5872dec
Android-HelloWorld <mongtamquoc2015@gmail.com>
2020-07-30 09:38:37 +0700

    Complete the LocationBox component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBox from './components/SearchBox';
+import LocationBox from './components/LocationBox';
+import WeatherBox from './components/WeatherBox';
 
 
 const api = {
@@ -9,10 +11,28 @@ const api = {
 
 
 function App() {
+
+	const [query, setQuery] = useState('');
+	const [weather, setWeather] = useState('');
+
+	const search = (event) => {
+		if (event.key === "Enter") {
+			fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+				.then(res => res.json())
+				.then(result => {
+					setWeather(result);
+					console.log(result);
+					setQuery('');
+				})
+		}
+	}
+
 	return (
 		<div className="app warm">
 			<main>
-				<SearchBox />
+				<SearchBox setQuery={setQuery} query={query} search={search} />
+				<LocationBox weather={weather} />
+				<WeatherBox />
 			</main>
 		</div>
 	);
---
+++ src/components/WeatherBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+export default function WeatherBox(props) {
+	return (
+		<div>	
+			<div className="weather-box">
+				<div className="temp"> 15C </div>
+				<div className="weather">Sunny</div>
+			</div>
+		</div>
+	)
+}
+
